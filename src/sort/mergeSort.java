85708135ffc9c4ac8d240f7e5971604155dfe39b
Lawrence Chung <chungl1@mcmaster.ca>
2018-02-24 18:39:06 -0500

    Recursive segment attempt

--- src/sort/mergeSort.java
+++ src/sort/mergeSort.java
@@ -2,4 +2,19 @@ package sort;
 
 public class mergeSort {
 
+	private static GeneralCompare[] aux;
+	
+	public static void merge(GeneralCompare[] gc, int n){
+		
+		aux = new GeneralCompare[n];
+		
+		if(n <= 1)
+			return;
+		int mid = n/2;
+		merge(gc, mid);
+		merge(gc, mid+1);
+		
+	}
+	
+	
 }
