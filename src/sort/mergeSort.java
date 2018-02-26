adc45061ed1089df90f18953a9f000468008c57c
Lawrence Chung <chungl1@mcmaster.ca>
2018-02-25 23:17:55 -0500

    Flawed mergeSort

--- src/sort/mergeSort.java
+++ src/sort/mergeSort.java
@@ -1,6 +1,6 @@
 package sort;
 
-public class mergeSort {
+public class mergeSort implements GeneralCompare{
 
 	private static GeneralCompare[] aux;
 	
@@ -18,8 +18,16 @@ public class mergeSort {
 			aux[i] = gc[i];
 		}
 		
+		for(int i = 1; i <= n; i++){
+			if(i > mid)
+				gc[i] = aux[mid+2];
+			else if(mid+1 > n)
+				gc[i] = aux[2];
+			else if(compare(aux[mid+1],aux[1]) < 0)
+				gc[i] = aux[i];
+		}
 		
 	}
-	
-	
+
+
 }
