7e78ae68ba9777707ae5c161a2cb285bfe0d8bd3
Lawrence Chung <chungl1@mcmaster.ca>
2018-02-26 03:17:35 -0500

    Questions about conditional and incorrect statements

--- src/sort/mergeSort.java
+++ src/sort/mergeSort.java
@@ -19,12 +19,14 @@ public class mergeSort implements GeneralCompare{
 		}
 		
 		for(int i = 1; i <= n; i++){
-			if(i > mid)
-				gc[i] = aux[mid+2];
-			else if(mid+1 > n)
+			if(i > mid) //Should these not be a[i] and a[mid]?
+				gc[i] = aux[mid++];
+			else if(mid+1 > n) //^
 				gc[i] = aux[2];
-			else if(compare(aux[mid+1],aux[1]) < 0)
+			else if(compare(aux[mid+1],aux[1]) < 0)// ?
 				gc[i] = aux[i]; //fill statement (incorrect)
+			else 
+				gc[i] = aux[2];
 		}
 		
 	}
