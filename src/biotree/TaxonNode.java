3414ad38bea6f5ab8536368785f951099d1ecb9b
Schankula Christopher <schankuc@mcmaster.ca>
2018-03-02 16:27:12 -0500

    function stubs for TaxonNode

--- src/biotree/TaxonNode.java
+++ src/biotree/TaxonNode.java
@@ -1,5 +1,32 @@
 package biotree;
 
 public class TaxonNode {
-
+	
+	
+	public void setParent(TaxonNode parent) {
+		
+	}
+	
+	public void addChild(TaxonNode child) {
+		
+	}
+	
+	public TaxonNode getParent() {
+		
+		return null;
+	}
+	
+	public TaxonNode[] getChildren() {
+		
+		return null;
+	}
+	
+	public int getCount() {
+		
+		return 0;
+	}
+	
+	public void incCount() {
+		
+	}
 }
