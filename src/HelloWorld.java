a326d16266ff5b506e3698d967691d51285ea341
Winnie <liang15@mcmaster.ca>
2018-03-01 19:52:42 -0500

    - Added shortcut println function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 
 public class HelloWorld {
+	// Abbreviated Print line Function
+	private static void println(String line) {
+		System.out.println(line);
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("I really want a monkey!");
 	}
