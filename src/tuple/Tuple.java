531f723319a1e6a5047a544f5483fd9ed2d7898c
Ray Liu <rayliu109@gmail.com>
2018-03-01 19:39:58 -0500

    Added third field to tuple

--- src/tuple/Tuple.java
+++ src/tuple/Tuple.java
@@ -1,12 +1,14 @@
 package tuple;
 
-public class Tuple<T1, T2> {
+public class Tuple<T1, T2, T3> {
 	T1 fst;
 	T2 snd;
+	T3 trd;
 	
-	public Tuple(T1 fst, T2 snd) {
+	public Tuple(T1 fst, T2 snd,T3 trd) {
 		this.fst = fst;
 		this.snd = snd;
+		this.trd= trd;
 	}
 	
 	public T1 fst() {
@@ -15,5 +17,8 @@ public class Tuple<T1, T2> {
 	
 	public T2 snd() {
 		return snd;
+	}
+	public T3 trd() {
+		return trd;
 	}
 }
