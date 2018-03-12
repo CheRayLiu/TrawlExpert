381e14ef5f4f064209baa7406ea956fd1024b53a
Schankula Christopher <schankuc@mcmaster.ca>
2018-03-12 18:01:07 -0400

    update DataStore to work with new Date object

--- src/data/DataStore.java
+++ src/data/DataStore.java
@@ -14,18 +14,7 @@ public class DataStore {
 	public static KDT<Record> records;
 	
 	public static void init(Record[] recs) {
-		GeneralCompare<Record> ax0 = (r0,r1) -> {
-			Record rec0 = (Record) r0;
-			Record rec1 = (Record) r1;
-			if (rec0.getYear() != rec1.getYear())
-				return rec0.getYear() - rec1.getYear();
-			else if (rec0.getMonth() != rec1.getMonth())
-				return rec0.getMonth() - rec1.getMonth();
-			else if (rec0.getDay() != rec1.getDay())
-				return rec0.getDay() - rec1.getDay();
-			else
-				return 0;
-		};
+		GeneralCompare<Record> ax0 = (r0,r1) -> ((Record) r0).getDate().compareTo(((Record) r1).getDate());
 		GeneralCompare<Record> ax1 = (r0,r1) -> ((Record) r0).getTaxonId() - ((Record) r1).getTaxonId();
 		GeneralCompare<Record> ax2 = (r0,r1) -> Float.compare(((Record) r0).getLatitude(), ((Record) r1).getLatitude());
 		GeneralCompare<Record> ax3 = (r0,r1) -> Float.compare(((Record) r0).getLongitude(), ((Record) r1).getLongitude());
