504946c932ffdf6eb9b93b13f9512c7b3cdf286c
Schankula Christopher <schankuc@mcmaster.ca>
2018-03-12 16:14:16 -0400

    infinite amount of searches

--- src/search/BasicSearch.java
+++ src/search/BasicSearch.java
@@ -12,23 +12,25 @@ import sort.RangeHelper;
 public class BasicSearch {
 	public static void init() {
 		System.out.println("======== TRAWLEXPERT ALPHA v1 ========");
-		System.out.print("Search by taxonId: ");
-		Scanner s = new Scanner(System.in);
-		int taxonId = s.nextInt();
-		
-		GeneralRange<Record> a0 = r -> 0;
-		GeneralRange<Record> a1 = r -> r.getTaxonId() - taxonId;//RangeHelper.taxonID(Bound.EQUALS, taxonId);
-		GeneralRange<Record> a2 = r -> 0;
-		GeneralRange<Record> a3 = r -> 0;
-		
-		ArrayList<GeneralRange<Record>> axes = new ArrayList<GeneralRange<Record>>();
-		
-		axes.add(a0);axes.add(a1);axes.add(a2);axes.add(a3);
-		
-		Iterable<Record> results = DataStore.records.rangeSearch(axes);
-		
-		for (Record r: results) {
-			System.out.println(r.getTaxonId());
+		while(true) {
+			System.out.print("Search by taxonId: ");
+			Scanner s = new Scanner(System.in);
+			int taxonId = s.nextInt();
+			
+			GeneralRange<Record> a0 = r -> 0;
+			GeneralRange<Record> a1 = r -> r.getTaxonId() - taxonId;//RangeHelper.taxonID(Bound.EQUALS, taxonId);
+			GeneralRange<Record> a2 = r -> 0;
+			GeneralRange<Record> a3 = r -> 0;
+			
+			ArrayList<GeneralRange<Record>> axes = new ArrayList<GeneralRange<Record>>();
+			
+			axes.add(a0);axes.add(a1);axes.add(a2);axes.add(a3);
+			
+			Iterable<Record> results = DataStore.records.rangeSearch(axes);
+			
+			for (Record r: results) {
+				System.out.println(r.getTaxonId());
+			}
 		}
 	}
 }
