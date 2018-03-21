dde3d200fe413fd34b20437ee923082c2619047e
Winnie <liang15@mcmaster.ca>
2018-03-21 10:07:24 -0400

    move tomcat to src

---
+++ src/tomcat/webapps/Trawl/script.js
@@ -0,0 +1,13 @@
+// For Range Slider
+$( function() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 1960,
+        max: 2016,
+        values: [ 1960, 2016 ],
+        slide: function( event, ui ) {
+            $( "#fromtoYear" ).html(ui.values[ 0 ] + " - " + ui.values[ 1 ] )
+        }
+    });
+    $( "#fromtoYear" ).html($( "#slider-range" ).slider( "values", 0 ) + " - " + $( "#slider-range" ).slider( "values", 1 ))
+} );
