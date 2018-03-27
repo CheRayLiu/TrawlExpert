76a4e5294c60cad282b4ce6c590bbf6de56db972
Lawrence Chung <chungl1@mcmaster.ca>
2018-03-27 14:39:33 -0400

    Added test cases CC.java

--- src/search/CC.java
+++ src/search/CC.java
@@ -7,13 +7,16 @@ public class CC {
 	private int count;
 	
 	public static void main(String[] args) {
-		Graph g = new Graph(5);
+		Graph g = new Graph(6);
 		g.addEdge(4, 3);
 		g.addEdge(2, 3);
 		g.addEdge(1, 2);
 		g.addEdge(0, 2);
 		CC component = new CC(g);
-		System.out.println(component.connected(0, 2));
+		System.out.println(component.connected(2, 0));
+		System.out.println(component.connected(0,1));
+		System.out.println(component.connected(1, 5));
+		System.out.println(component.connected(1,6)); //throws exception (supposed to)
 	}
 	
 	public CC(Graph G){
