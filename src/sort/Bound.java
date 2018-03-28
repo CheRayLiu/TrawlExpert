0d739adfc806ebd599016dc48490af6107449950
Ray Liu <rayliu109@gmail.com>
2018-03-28 08:26:23 -0400

    Fixed merge errors

--- src/sort/Bound.java
+++ src/sort/Bound.java
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
+
 package sort;
 /**
  * Enumeration of bound types.
  * @author Christopher W. Schankula
  *
  */
-=======
-package sort;
 
->>>>>>> b093c71758057e7a5b83b69a7632a229ad1469bf
+
 public enum Bound {
 	LOWER, UPPER, LOWHIGH, ANY, EQUALS;
 }
