9edd9c9ae9ad5a84e36f54394e8ce45590297b0c
Lawrence Chung <chungl1@mcmaster.ca>
2018-03-28 09:00:00 -0400

    Added off-by-one test cases

--- src/test/TestGraph.java
+++ src/test/TestGraph.java
@@ -15,18 +15,30 @@ public class TestGraph {
 	public void testV(){
 		Graph g = new Graph(8);
 		assertEquals(g.V(), 8);
+		assertNotEquals(g.V(), 7);
+		assertNotEquals(g.V(), 9);
 	}
 	
 	@Test
-	public void testE(){
+	public void testAddEdgeAndE(){
 		Graph g = new Graph(8);
 		g.addEdge(4, 3);
 		g.addEdge(2, 3);
 		g.addEdge(1, 2);
 		g.addEdge(0, 2);
+		assertNotEquals(g.E(), 3);
 		assertEquals(g.E(), 4);
 		g.addEdge(7, 2);
 		assertNotEquals(g.E(), 4);
+		assertEquals(g.E(), 5);
 	}
 	
+	@Test
+	public void adj(){
+		Graph g = new Graph(8);
+		g.addEdge(4, 3);
+		g.addEdge(2, 3);
+		g.addEdge(1, 2);
+		g.addEdge(0, 2);
+	}
 }
