f85ddf8f520385e02ffa7f089e1c32611d3c68fd
Haley Glavina <HaleyGlavina@Haleys-MacBook-Pro.local>
2018-04-03 18:11:50 -0400

    design spec changes

--- src/search/Field.java
+++ src/search/Field.java
@@ -1,6 +1,7 @@
 package search;
 
 // The interface for a function that returns one field (piece of data) from a record
+
 public interface Field<Key extends Comparable<Key>, Value> {
 	public Key field(Value a1);
 }
